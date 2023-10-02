2198c1491fb2c4737241031cb2018cb4f6862e19
Mike_Cupples <mikecupples@gravityassist77.com>
2023-10-02 05:41:38 -0700

    Uncommented lines to test routine.  This is really a test as I gain understanding on how to use Git from PyCharm

--- Courseware-TDD/labs/splitting.py
+++ Courseware-TDD/labs/splitting.py
@@ -1,5 +1,6 @@
 # trying out automated testing ideas/routines
 
+
 def split_amount(amount, n):
     portion, remain = amount // n, amount % n
     portions = []
@@ -11,11 +12,11 @@ def split_amount(amount, n):
     return portions
 
 
-# split = split_amount(5, 3)
-# print(split)
-#
-# split = split_amount(5, 4)
-# print(split)
-#
-# split = split_amount(9, 8)
-# print(split)
+split = split_amount(5, 3)
+print(split)
+
+split = split_amount(5, 4)
+print(split)
+
+split = split_amount(9, 8)
+print(split)
