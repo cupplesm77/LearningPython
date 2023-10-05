6e32af96e0f69b8773be74e432b66dc2a83d9b27
Mike_Cupples <mikecupples@gravityassist77.com>
2023-10-05 08:19:28 -0700

    added a new working file: misc.py

--- Courseware-TDD/labs/misc.py
+++ Courseware-TDD/labs/misc.py
@@ -1,5 +1,7 @@
 # misc.py
 
+import re
+
 # count the number of sentences in a simple sentence
 text = """This is a longer story.
 
@@ -15,3 +17,36 @@ print(f"string with new-lines removed = {text_replace}")
 
 text_count_sentences = 1 + text_replace.count(". ")
 print(f"number of sentences in text_replace = {text_count_sentences}")
+
+
+# more regex
+
+# regex for ONLY whitespace
+pattern = "^\s*$"
+
+# string with only whitespace
+text1 = """             
+            
+        """
+print(text1)
+text1_search = re.search(pattern, text1)
+print(f"text1_search = {text1_search}")
+print("")
+if text1_search:
+    raise ValueError("Text must not be only whitespace")
+else:
+    print("Terrific! Text is not ONLY whitespace")
+
+
+# a string with not only whitespace
+text2 = """1st line             
+
+        """
+print(text2)
+text2_search = re.search(pattern, text2)
+print(f"text2_search = {text2_search}")
+print("")
+if text2_search:
+    raise ValueError("Text must not be only whitespace")
+else:
+    print("Terrific! Text is not ONLY whitespace")
