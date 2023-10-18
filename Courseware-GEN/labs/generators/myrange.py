a50f3faec3ae242bc95b66e31b35752b91e17090
Mike_Cupples <mikecupples@gravityassist77.com>
2023-10-18 13:35:28 -0700

    Incorporated enumeration type parameters into the code: e.g. ONE = 1, TWO = 2, etc. Fixed testing cases in test_myrange.py to check for the correct error:  TypeError and ValueError correctly delineated.

--- Courseware-GEN/labs/generators/myrange.py
+++ Courseware-GEN/labs/generators/myrange.py
@@ -1,25 +1,31 @@
 # myrange.py
 
+from enum import Enum
+
 def myrange(*args):
     """
 
     :param args: int
     :return: int
     """
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+
     # error checking on the args
-    print(f"myrange args are {args}")
-    len_args = len(args)
+    numArgs = len(args)
     # ValueErrors
-    if len_args == 0:
-        raise ValueError(f"myrange must have at least one argument: myrange args are {args}")
-    if len_args > 3:
-        raise ValueError(f"myrange can not have more than 3 args")
-    if len_args == 2 and args[0] > args[1]:
+    if numArgs == ZERO:
+        raise TypeError(f"myrange expected at least 1 argument, got {numArgs}")
+    if numArgs > THREE:
+        raise TypeError(f"myrange expected at most 3 arguments, got {numArgs}")
+    if numArgs == TWO and args[ZERO] > args[ONE]:
         raise ValueError(f"'start' cannot be greater than 'end': myrange args are {args}")
-    if len_args == 3 and args[2] > 0 and args[0] > args[1]:
-        raise ValueError(f"'start' cannot be greater than 'end': myrange args are {args}")
-    if len_args == 3 and args[2] < 0 and args[1] > args[0]:
-        raise ValueError(f"'start' cannot be greater than 'end': myrange args are {args}")
+    if numArgs == THREE and args[TWO] > ZERO and args[ZERO] > args[ONE]:
+        raise ValueError(f"For counting up, 'start' cannot be greater than 'end': myrange args are {args}")
+    if numArgs == THREE and args[TWO] < ZERO and args[ONE] > args[ZERO]:
+        raise ValueError(f"For counting down, 'start' cannot be less than 'end': myrange args are {args}")
     # TypeErrors
     for arg in args:
         if not isinstance(arg, int):
@@ -27,30 +33,30 @@ def myrange(*args):
 
     # set up the start, stop, and step values for the "myrange" function
 
-    if len_args == 1:     # myrange has one argument
-        start = 0
-        end = args[0]
+    if numArgs == ONE:     # myrange has one argument
+        start = ZERO
+        end = args[ZERO]
+        step = ONE
+    elif numArgs == TWO:   # myrange has two arguments
+        start = args[ZERO]
+        end = args[ONE]
         step = 1
-    elif len_args == 2:   # myrange has two arguments
-        start = args[0]
-        end = args[1]
-        step = 1
-    elif len_args == 3:   # myrange has three arguments
-        start = args[0]
-        end = args[1]
-        step = args[2]
+    elif numArgs == THREE:   # myrange has three arguments
+        start = args[ZERO]
+        end = args[ONE]
+        step = args[TWO]
     else:                 # myrange has greater than three arguments
         return "myrange generator function has more than three args"
 
     # counting up from a start value to an "end" value
-    if step > 0:
+    if step > ZERO:
         n = start
         while n < end:
             yield n
             n += step
 
     # counting down from a start value to an end value
-    if step < 0:
+    if step < ZERO:
         n = start
         while n > end:
             yield n
