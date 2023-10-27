81d8a8ce2dd8dec78496c18be50f90635419c0d0
Mike_Cupples <mikecupples@gravityassist77.com>
2023-10-27 14:59:20 -0700

    Scratch work for args, *args, and **kwargs

---
+++ Courseware-NXT/labs/varargs1.py
@@ -0,0 +1,72 @@
+# varargs1.py
+
+def print_args(*args):
+    for arg in args:
+        print(arg)
+
+def print_kwargs(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key} -> {value}")
+
+def print_all(*args, **kwargs):
+    for arg in args:
+        print(arg)
+    for key, value in kwargs.items():
+        print(f"{key} -> {value}")
+
+def add_to_dict(stuff, *args, **kwargs):
+    for key, value in kwargs.items():
+        if key not in stuff:
+            stuff[key] = value
+
+    return stuff
+
+def normal_function(a, b, c):
+    print(f"a: {a} b: {b} c: {c}")
+
+numbers = (3, 2, 1)
+
+normal_function(*numbers)
+
+other_numbers = {'a': 1, 'b': 1, 'c': 1}
+
+normal_function(**other_numbers)
+
+def order_book(title, author, isbn):
+    """
+
+    :param title:
+    :param author:
+    :param isbn:
+    :return:
+    """
+    print(f'Ordering "{title}" by {author} ({isbn})')
+
+
+def get_required_textbook(class_id):
+    """
+
+    :param class_id:
+    :return:
+    """
+    return "Coloring with Great Enthusiasm", "Joseph Nash", 22447788
+
+
+book = get_required_textbook(4242)
+print(book)
+order_book(*book)
+
+
+print_args('red', 'blue', 'green')
+print("")
+print_args()
+print('')
+print_args(' ')
+print('')
+print_kwargs(x=4, y=10)
+print('')
+print_all(4, 5, 6, z=15, x='test')
+print_all(4, 5)
+print_all(x='test2', y=0.99)
+print('')
+print(add_to_dict({}, 2, 3, x=10, y='test3'))
