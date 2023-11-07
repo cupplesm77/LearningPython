f8d52406d27a69567ccf4232e34e03c652287a6a
Mike_Cupples <mikecupples@gravityassist77.com>
2023-11-07 11:31:06 -0800

    Supporting decorator scratch work.

--- Courseware-NXT/labs/decorators/decorators1.py
+++ Courseware-NXT/labs/decorators/decorators1.py
@@ -1,5 +1,31 @@
 # decorators1.py
 
+def shout(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs).upper()
+    return wrapper
+
+@shout
+def say_hello(who):
+    return f"Hello, {who}!"
+
+print(say_hello("George"))
+
+# Masking
+def check_id(func):
+    def wrapper(*args, **kwargs):
+        print("ID of func: " + str(id(func)))
+        return func(*args, **kwargs)
+    print("ID of wrapper: " + str(id(wrapper)))
+    return wrapper
+
+
+@check_id
+def f(x):
+    return x * 3
+
+f(2)
+
 
 def printlog(func):
     def wrapper(*arg, **kwargs):
