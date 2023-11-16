9d28e3b56e05646ca89f5de3b100711fd5cf4238
Mike_Cupples <mikecupples@gravityassist77.com>
2023-11-15 18:21:52 -0800

    Various changes related to learning in Powerful Python, in particular @classmethod

--- Courseware-OOP/labs/classmethod.py
+++ Courseware-OOP/labs/classmethod.py
@@ -3,7 +3,7 @@
 Created on Sat Aug 19 21:36:09 2023
 
 I attempted to incorporate @classmethod for two use cases:
-    1) alternate constructors, e.g. interface_one and interface_two
+    1) alternate constructors, e.g. constructor_one and constructor_two
     2) changing class variables, e.g. change_default_age, etc.
 
 Want to get a quick code review of my set of simple classes that apply classmethods.
@@ -17,112 +17,129 @@ what about encapsulation?
 
 What about my use of inheritance?
 
+What about the Liskov Principle?
+
 Using default class variables as method default parameter values?
 
 """
 
+from dataclasses import dataclass, field
+
+
 # ************************ ClassMethods ************************************
-
-class Officer():
-    RANK_DEFAULT = "Colonel"
-    NAME_DEFAULT = "John Wayne"
-    AGE_DEFAULT = 37
-
-    def __init__(self, name=NAME_DEFAULT, age=AGE_DEFAULT, rank=RANK_DEFAULT):
-        self.name = name
-        self.age = age
-        self.rank = rank
+USE_DEFAULT_RANK = object()
+GLOBAL_RANK_DEFAULT = "First Lieutenant"
+@dataclass
+class Officer:
+    name_default = "John Wayne"
+    age_default = 37
+    rank_default = "Colonel"
+    """
+        Toy classes illustrating application of best practices using @classmethod
+    """
+    name: str = field(default=name_default)
+    age: int = field(default=age_default)
+    rank: str = field(default=rank_default)
 
     @classmethod
-    def interface_one(cls, last_name=NAME_DEFAULT, age=AGE_DEFAULT, rank=RANK_DEFAULT):
+    def constructor_one(cls, last_name=name_default, age=age_default, rank=rank_default):
         """
         Alternative constructor
         Parameters
+            last_name: str   should use last name only for name
         """
         name = last_name
         return cls(name, age, rank)
 
     @classmethod
-    def interface_two(cls, name, rank):
+    def constructor_two(cls, name, rank):
         """
-        An alternative contructor:
-        A different interface than interface_one and the baseclass interface
+        An alternative constructor:
+        A different interface than constructor_one and the baseclass interface
         Only includes name and rank call list parameters
         """
-        name = name
-        age = cls.AGE_DEFAULT
-        rank = rank
-        return cls(name, age, rank)
+        return cls(name=name, rank=rank)   # note that the age is defaulted to officer age_default
 
-    @classmethod
-    def change_default_age(cls, new_age):
-        """
-        Changing class default parameter: age
-        """
-        cls.AGE_DEFAULT = new_age
-
-    @classmethod
-    def change_default_rank(cls, new_rank):
-        """
-        Changing class default parameter: rank
-        """
-        cls.RANK_DEFAULT = new_rank
 
 class Marine(Officer):
+    name_default = "Tony Curtis"
+    age_default = 35
+    rank_default = "Captain"
+
     @classmethod
-    def interface_one(cls, name=Officer.NAME_DEFAULT, rank=Officer.RANK_DEFAULT, age=Officer.AGE_DEFAULT):
+    # def constructor_one(cls, name=name_default, rank=rank_default, age=age_default):  # Follow the Liskov Substitution Principle
+    def constructor_one(cls, name=name_default, age=age_default, rank=USE_DEFAULT_RANK):
+        if rank is USE_DEFAULT_RANK:
+            rank = GLOBAL_RANK_DEFAULT
+        else:
+            rank = rank
         return cls(name, age, rank)
 
-
+@dataclass
 class Army(Officer):
+    name_default = "Loyd Bridges"
+    age_default = 31
+    rank_default = "First Lieutenant"
+
+    name: str = field(default=name_default)
+    age: int = field(default=age_default)
+    rank: str = field(default=rank_default)
+
     @classmethod
-    def interface_two(cls, name=Officer.NAME_DEFAULT, rank=Officer.RANK_DEFAULT):
-        age = cls.AGE_DEFAULT
-        return cls(name, age, rank)
+    def constructor_two(cls, name=name_default, rank=rank_default):  # note the implicit use of default age from officer
+        return super().constructor_two(name, rank)
+
+    @classmethod
+    def constructor_three(cls, name=name_default, rank=rank_default):  # note the implicit use of default age
+        return cls(name=name, rank=rank)
 
 
-marine_officer0 = Marine.interface_one()
+marine_officer0 = Marine.constructor_one()
 print(marine_officer0.name)
 print(marine_officer0.age)
 print(marine_officer0.rank)
 
 print("")
-army_officer0 = Army.interface_one()
+army_officer0 = Army.constructor_one()
 print(army_officer0.name)
 print(army_officer0.age)
 print(army_officer0.rank)
 
-officer1 = Marine.interface_one("Cupples", "Colonel", 41)
+officer1 = Marine.constructor_one("Cupples", 42, "Colonel")
 print(officer1.name)
 print(officer1.age)
 print(officer1.rank)
 print(f"Officer1's first age: {officer1.age}")
-officer1.age = officer1.AGE_DEFAULT
+
 print(f"Officer1's revised age: {officer1.age}")
-officer1.change_default_age(47)
-officer1.age = officer1.AGE_DEFAULT
 print(f"Officer1's revised age: {officer1.age}")
 officer1.age = 41
 print(f"Officer1's revised age: {officer1.age}")
 
 print("")
-officer2 = Marine.interface_one("Curtis", "Major", 29)
+officer2 = Marine.constructor_one("Jones", 29, "Major")
 print(officer2.name)
 print(officer2.rank)
 print(officer2.age)
 print(f"officer2's current age: {officer2.age}")
-print(f"officer2's AGE_DEFAULT age: {officer2.AGE_DEFAULT}")
-officer2.change_default_rank("General")
-officer2.rank = officer2.RANK_DEFAULT
-officer2.age = officer2.AGE_DEFAULT
 print(f"Officer2's revised rank: {officer2.rank}")
 print(f"Officer2's revised age: {officer2.age}")
 
 print("")
-officer3 = Army.interface_two("Jones", "Colonel")
-print(officer3.name)
-print(officer3.rank)
-print(officer3.age)
-print(f"officer2's current age: {officer3.age}")
+officer3 = Army.constructor_two("Roger Stevens", "General")
+print(f"officer3 is of type: {type(officer3)}")
+print(f"officer3's name: {officer3.name}")
+print(f"officer3's rank: {officer3.rank}")
+print(f"officer3's age: {officer3.age}")
+
+print("")
+officer4 = Army.constructor_three("Timothy Cupples", "Colonel")
+print(f"officer4 is of type: {type(officer4)}")
+print(f"officer4's name: {officer4.name}")
+print(f"officer4's rank: {officer4.rank}")
+try:
+    print(f"officer4's age: {officer4.age}")
+except NameError:
+    print("constructor_two does not have an age attribute")
 
 # ************************ ClassMethods: END ************************************
