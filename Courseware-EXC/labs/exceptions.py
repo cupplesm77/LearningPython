4a7c5524cf8c0cf32f2ef66a03f1d3fdc8a99595
Mike_Cupples <mikecupples@gravityassist77.com>
2023-12-13 11:39:38 -0800

    Competed exceptions.py All tests completed successfully. Added a couple of files not committed properly.

--- Courseware-EXC/labs/exceptions.py
+++ Courseware-EXC/labs/exceptions.py
@@ -86,7 +86,44 @@ ValueError: Invalid format: 415-555
 '''
 
 # Write your code here:
+def get_index_or_default(color_array, index, default_color):
+    try:
+        color = color_array[index]
+    except IndexError:
+        return default_color
+    return color
 
+def parse_name(name):
+    name_types = ['first', 'last']
+    names = name.split(" ")
+    if len(names) != 2:
+        raise ValueError("Cannot split name: " + str(name))
+
+    return dict(zip(name_types, names))
+
+
+def parse_phone_number(phone_number):
+    number_types = ['area', 'exchange', 'last4']
+    phone_numbers = phone_number.split("-")
+
+    if len(phone_numbers) != 3:
+        raise ValueError("Invalid format: {}".format(phone_number))
+
+    area_len = len([c for c in phone_numbers[0]])
+    if area_len != 3:
+        raise ValueError("Invalid format: {}".format(phone_number))
+
+    exchange_len = len([c for c in phone_numbers[1]])
+    if exchange_len != 3:
+        raise ValueError("Invalid format: {}".format(phone_number))
+
+    last4_len = len([c for c in phone_numbers[2]])
+    if last4_len != 4:
+        raise ValueError("Invalid format: {}".format(phone_number))
+
+    phone_number_zip = zip(number_types, phone_numbers)
+    phone_number_dict = dict(phone_number_zip)
+    return phone_number_dict
 
 
 # Do not edit any code below this line!
